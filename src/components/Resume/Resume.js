a625dd4f82d1f4c8ff51b7a1412541532dc4c8d9
Peter Sekesan <psekesan@gmail.com>
2021-07-15 18:09:14 -0400

    updates resume

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -4,7 +4,7 @@ import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
 import Resumecontent from "./ResumeContent";
 import axios from "axios";
-import pdf from "../../Assets/Soumyajit-Behera.pdf";
+import pdf from "../../Assets/peterSekesanResumeV2.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 
 function Resume() {
@@ -42,45 +42,44 @@ function Resume() {
           <Col md={6} className="resume-left">
             <h3 className="resume-title">Experience</h3>
             <Resumecontent
-              title="JUNIOR ML ENGINEER [Omdena]"
-              date="June 2020 - August 2020"
+              title="Frontend UI Engineer [Black Lake Labs]"
+              date="May 2021 - Present"
               content={[
-                "Assembled the data from various social media platforms using Twitter, Reddit.Interpreted the collected text using word-clouds and various other factors that affect the change of sentiments of youth.",
-                " Utilized the data to find the issues using Topic Modelling and Building models using LSTM, BERT to predict the sentiments of the youth.",
+                "Coded alongside lead developer to overhaul UI built with React in conjunction with Figma wireframes",
+                "Partner with Project Managers on new feature ideation and experiment design",
+              ]}
+            />
+            <Resumecontent
+              title="Events Management Specialist [CANON USA]"
+              date="Oct 2012 - May 2020"
+              content={[
+                "Leveraged leadership expertise to direct all aspects of major media and trade show events yielding 85% increase in market share",
+                "Led teams of up to nine for major events such as Super Bowl, Kentucky Derby and Indy 500",
               ]}
             />
             <h3 className="resume-title">Extracurricular Activities</h3>
             <Resumecontent
-              title="Web Developer [Pantheon-2019 Technical Fest of BIT Mesra]"
+              title="JSworld Conference [Amsterdam-2021 Frontend Love]"
               content={[
-                "Worked on creating the frontend-end of the website using Bootstrap, Javascript.",
+                "Participated in three day long intensive workshops surrounding all thing Javascript",
               ]}
             />
-            <Resumecontent
-              title="Web Developer [Bitotsav-2020 Technical Fest of BIT Mesra]"
-              content={[
-                "Operated on developing the frontend end of the website using Bootstrap, Javascript and backend APIs using Node.js",
-              ]}
-            />
+            
           </Col>
           <Col md={6} className="resume-right">
             <h3 className="resume-title">Education</h3>
             <Resumecontent
-              title="IMSC MATHS AND COMPUTING [BIT Mesra, Ranchi] "
-              date="2018 - Present"
-              content={[`CGPA: ${cgpa} (Till ${sem}th Sem)`]}
+              title="Software Engineering Program [Thinkful - Brookyln] "
+              date="Oct 2020 - April 2021"
+              content={[`Certification`]}
             />
             <Resumecontent
-              title="12TH BOARD [ODM Public School,Odisha]"
-              date="2015 - 2017"
-              content={["Precentage: 88%"]}
+              title="Marketing [Nassau Community College - Garden City,NY]"
+           
+              content={[]}
             />
-            <Resumecontent
-              title="10TH BOARD [ST Mary's School,Odisha] "
-              date="2005 - 2015"
-              content={["Precentage: 86%"]}
-            />
-            <h3 className="resume-title">Ranks and Achivements</h3>
+            
+            {/* <h3 className="resume-title">Ranks and Achivements</h3>
             <Resumecontent
               title=""
               content={[
@@ -89,7 +88,7 @@ function Resume() {
                 "Top Performer in Code-Break 1.0",
                 "Participant in Hack-A-Bit 2019",
               ]}
-            />
+            /> */}
           </Col>
         </Row>
         <Row style={{ justifyContent: "center", position: "relative" }}>
