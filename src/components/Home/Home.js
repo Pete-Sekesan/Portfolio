7a6ea886a625bf31d706516988a808bc083a5acf
Peter Sekesan <psekesan@gmail.com>
2021-07-16 17:44:56 -0400

    style done

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import homeLogo from "../../Assets/pete-windsor-775x775.jpg";
+import homeLogo from "../../Assets/pete-rome.jpg";
 import Particle from "../Particle";
 import Home2 from "./Home2";
 import Type from "./Type";
