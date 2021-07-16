19e6c649075f746e45d8fcf5a3df18effde1a87a
Peter Sekesan <psekesan@gmail.com>
2021-07-16 12:17:15 -0400

    projects updated

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -47,7 +47,7 @@ function Home2() {
               </i>
               &nbsp; like
               <i>
-                <b className="purple"> React.js and Gatsby.js</b>
+                <b className="purple"> React.js, jQuery and Gatsby.js</b>
               </i>
             </p>
           </Col>
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -3,12 +3,10 @@ import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
 
-import leaf from "../../Assets/Projects/leaf.png";
-import emotion from "../../Assets/Projects/emotion.jpeg";
-import editor from "../../Assets/Projects/codeEditor.png";
-import chatify from "../../Assets/Projects/chatify.png";
-import suicide from "../../Assets/Projects/suicide.png";
-import bitsOfCode from "../../Assets/Projects/blog.png";
+import ghf from "../../Assets/Projects/github-finder-demo.gif";
+import noy from "../../Assets/Projects/noy-gif.gif";
+import myf from "../../Assets/Projects/myf-demo.gif";
+import dis from "../../Assets/Projects/disney-quiz-demo.gif";
 
 function Projects() {
   return (
@@ -16,7 +14,7 @@ function Projects() {
       <Particle />
       <Container>
         <h1 className="project-heading">
-          My Recent <strong className="purple">Works </strong>
+          My latest <strong className="purple">Works </strong>
         </h1>
         <p style={{ color: "white" }}>
           Here are a few projects I've worked on recently.
@@ -24,62 +22,41 @@ function Projects() {
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={chatify}
+              imgPath={myf}
               isBlog={false}
-              title="Chatify"
-              description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
-              link="https://github.com/soumyajit4419/Chatify"
+              title="Mind Your Fitness"
+              description="This app was created to allow users to have a small, standalone application to log and keep track of their exercise's and to view them all in one central location. A full stack application that utilizes React.js for fast and responsive rendering, this application points to a Node.js/Express server that connects to a PostgreSQL database using Knex as the query builder."
+              link="https://github.com/Pete-Sekesan/mind-your-fitness"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={leaf}
+              imgPath={ghf}
               isBlog={false}
-              title="Plant AI"
-              description="Used the plant disease dataset from Kaggle and trained a image classifer model using 'PyTorch' framework using CNN and Transfer Learning with 38 classes of various plant leaves. The model was successfully able to detect diseased and healthy leaves of 14 unique plants. I was able to achieve an accuracy of 98% by using Resnet34 pretrained model."
-              link="https://github.com/soumyajit4419/Plant_AI"
+              title="Github Finder"
+              description="React application to search through GitHub users. The application uses the GitHub Users API to make a fetch call to pull in data of any user searched for. Using React with Hooks to render the client side using functional components and Context for state management, the data creates personalized bio page of each user. This application was originally written using class components, which I then refactored to functional components to learn a new skill."
+              link="https://github.com/Pete-Sekesan/github-finder"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={editor}
+              imgPath={noy}
               isBlog={false}
-              title="Editor.io"
-              description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"
-              link="https://github.com/soumyajit4419/Editor.io"
+              title="News of Yesteryear"
+              description="This is an app based on my love for microfilm. Utilizing the Library of Congresses Chronicling America API, you can search any topic be presented with digital scans of newspapers pertaining to that term dating from 1963 all the way back to 1789! The technology stack for this app included HTML, CSS, JavaScript, jQuery and Restful APIs."
+              link="https://github.com/Pete-Sekesan/News-of-Yesteryear"
             />
           </Col>
-
+         
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={bitsOfCode}
+              imgPath={dis}
               isBlog={false}
-              title="Bits-0f-C0de"
-              description="My personal blog page build with Next.js and Tailwind Css which takes the content from makdown files and renders it using Next.js. Supports dark mode and easy to write blogs using markdown."
-              link="https://github.com/soumyajit4419/Bits-0f-C0de"
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={suicide}
-              isBlog={false}
-              title="Ai For Social Good"
-              description="Using 'Natural Launguage Processing' for the detection of suicide-related posts and user's suicide ideation in cyberspace  and thus helping in sucide prevention."
-              link="https://github.com/soumyajit4419/AI_For_Social_Good"
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={emotion}
-              isBlog={false}
-              title="Face Recognition and Emotion Detection"
-              description="Trained a CNN classifier using 'FER-2013 dataset' with Keras and tensorflow backened. The classifier sucessfully predicted the various types of emotions of human. And the highest accuracy obtained with the model was 60.1%.
-              Then used Open-CV to detect the face in an image and then pass the face to the classifer to predict the emotion of a person."
-              link="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
+              title="Disney Quiz App"
+              description="This was one of my first projects to create in my program. A short quiz app dedicated to one of my favorite subjects, Disney Parks! The technology stack for this app included HTML, CSS, JavaScript and jQuery."
+              link="https://github.com/Pete-Sekesan/disney-quiz-app"
             />
           </Col>
         </Row>
