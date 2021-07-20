275aaa0d326235aef459bd04d7dd9eb8dc4a6ca2
Peter Sekesan <psekesan@gmail.com>
2021-07-20 13:02:44 -0400

    added descriptions to tech tools card

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -18,27 +18,35 @@ function Techstack() {
       
       <Col xs={4} md={2} className="tech-icons">
         <DiJavascript1 />
+        <p className="tech-desc">Javascript</p>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <DiNodejs />
+        <p className="tech-desc">Node.js</p>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <DiReact />
+        <p className="tech-desc">React</p>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <DiPostgresql />
+        <p className="tech-desc">PostgreSQL</p>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <DiJqueryLogo />
+        <p className="tech-desc">jQuery</p>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <DiGit />
+        <p className="tech-desc">Git</p>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiGatsby />
+        <p className="tech-desc">Gatsby</p>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <DiCss3Full />
+        <p className="tech-desc">CSS</p>
       </Col>
     </Row>
   );
--- src/components/About/Toolstack.js
+++ src/components/About/Toolstack.js
@@ -13,18 +13,23 @@ function Toolstack() {
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
       <Col xs={4} md={2} className="tech-icons">
         <SiGithub />
+        <p className="tech-desc">GitHub</p>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiVisualstudiocode />
+        <p className="tech-desc">VS Code</p>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiNetlify />
+        <p className="tech-desc">Netlify</p>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiPostman />
+        <p className="tech-desc">Postman</p>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiHeroku />
+        <p className="tech-desc">Heroku</p>
       </Col>
     </Row>
   );
